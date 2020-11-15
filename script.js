4507fcfee77b66449f21e44444c4f09bb7207b2d
Jorge Mitri <mitri.dvp@gmail.com>
2020-11-15 18:55:52 -0300

    Visualizacion añadida

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ btns[1].addEventListener('click', () => {
   input.value = '3+((6+3/4)-5+6)'
 })
 btns[2].addEventListener('click', () => {
-  input.value = '5+6+7+8+8(2/1)'
+  input.value = '5+6+7+8(2/1)'
 })
 
 // Functions
@@ -90,37 +90,103 @@ function parse(e) {
   parseTree(result)
 }
 
-function Node(value, left = null, right = null) {
-  this.value = value
-  this.left = left
-  this.right = right
-
-  // this.z = z;
-}
-
 function parseTree(postfix) {
   const nodeList = []
-  // let z = 0
+  let y = 0
   for (let i = 0; i < postfix.length; i++) {
 
     const e = postfix[i];
     
     if(NUMBER.includes(+e)) {
-      nodeList.push(new Node(e))
+      nodeList.push(new NumberNode(e, y))
     }
 
     if(OP.includes(e)) {
       const left = nodeList.pop()
+      left.y = y
       const right = nodeList.pop()
-      // z++
-      nodeList.push(new Node(e, left, right))
+      right.y = y
+      y++
+      nodeList.push(new Node(e, right, left, y, y*2))
     }    
   }
-  output.innerHTML += '<h2>Arbol</h2><div>'+JSON.stringify(nodeList[0])+'</div> <span>-Se recomienda visualizar arbol en consola-</span>' 
+  output.innerHTML += '<h2>Arbol</h2><div>'+nodeList[0].rep()+'</div>' 
   displayTree(nodeList[0])
 }
 
+function Node(value, left = null, right = null, y = 0, x = 0) {
+  this.value = value
+  this.left = left
+  this.right = right
+
+  this.y = y
+  this.x = x
+
+  // this.z = z;
+  this.rep = () => {
+    return `(${this.left.rep()}, ${this.value}, ${this.right.rep()})`;
+  }
+
+  this.actualizarX = (x) => {
+    this.x = x;
+    this.left.actualizarX(x - numeroPar(y))
+    this.right.actualizarX(x + numeroPar(y))
+  }
+
+  this.render = () => {
+    const renderDOM = document.querySelector('.render')
+    const div = document.createElement('div')
+    div.textContent = this.value
+    div.classList.add('node')
+    div.style.top = -this.y + 'em'
+    div.style.left = this.x + 'em'
+
+    renderDOM.appendChild(div)
+
+    this.left.render()
+    this.right.render()
+  } 
+}
+function NumberNode (n, y = 0, x = 0) {
+  this.value = n
+  this.x = x;
+  this.y = y
+  
+  this.rep = () => {
+    return `${this.value}`
+  }
+
+  this.actualizarX = (x) => {
+    this.x = x;
+  }
+
+  this.render = () => {
+    const renderDOM = document.querySelector('.render')
+    const div = document.createElement('div')
+    div.textContent = this.value
+    div.classList.add('node')
+    div.style.top = -this.y + 'em'
+    div.style.left = this.x + 'em'
+    renderDOM.appendChild(div)
+  }
+}
+
 function displayTree(tree) {
+  const renderDOM = document.querySelector('.render')
+  renderDOM.innerHTML = ''
   console.log(tree)
+  tree.actualizarX(tree.y)
+  tree.render()
+}
+
+function numeroPar(posicion) {
+  if(posicion === 1) return 1
+  let i = 2
+  let n = 0
+  do {
+    n = n + 2
+    i++
+  } while (i <= posicion);
+  return n
 
 }
